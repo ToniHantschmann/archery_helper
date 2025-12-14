c4e06461207f081c7003d0ef3f9e5265911486d3
ToniHantschmann <antonhantschmann@yahoo.de>
2025-12-14 10:29:30 +0100

    added keyboard provider

---
+++ lib/providers/keyboard_config_provider.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/keyboard_config.dart';
+
+/// Provider für die Keyboard-Konfiguration
+class KeyboardConfigNotifier extends StateNotifier<KeyboardConfig> {
+  KeyboardConfigNotifier() : super(KeyboardConfig.defaults());
+
+  /// Setzt eine neue Tastenbelegung
+  void setKeyBinding(LogicalKeyboardKey key, AppAction action) {
+    state = state.addKeyBinding(key, action);
+    // TODO: Später in SharedPreferences speichern
+  }
+
+  /// Entfernt eine Tastenbelegung
+  void removeKeyBinding(LogicalKeyboardKey key) {
+    state = state.removeKeyBinding(key);
+    // TODO: Später in SharedPreferences speichern
+  }
+
+  /// Setzt die Tastenbelegung auf Standard zurück
+  void resetToDefaults() {
+    state = KeyboardConfig.defaults();
+    // TODO: Später aus SharedPreferences löschen
+  }
+
+  /// Lädt die gespeicherte Konfiguration
+  Future<void> loadConfig() async {
+    // TODO: Aus SharedPreferences laden
+    // Für jetzt verwenden wir die Defaults
+  }
+}
+
+/// Haupt-Provider für Keyboard-Konfiguration
+final keyboardConfigProvider =
+    StateNotifierProvider<KeyboardConfigNotifier, KeyboardConfig>(
+  (ref) => KeyboardConfigNotifier(),
+);
+
+/// Convenience Provider: Gibt die Aktion für eine Taste zurück
+final keyActionProvider = Provider.family<AppAction?, LogicalKeyboardKey>(
+  (ref, key) {
+    return ref.watch(keyboardConfigProvider).getAction(key);
+  },
+);
