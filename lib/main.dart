ca9df34985862949179ed4ba9b4508be96111611
ToniHantschmann <antonhantschmann@yahoo.de>
2026-01-05 22:52:46 +0100

    using and loading shared preferences in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:archery_helper/providers/keyboard_config_provider.dart';
+import 'package:archery_helper/providers/settings_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/app.dart';
 
-void main() {
-  runApp(const ProviderScope(child: ArcheryHelperApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final container = ProviderContainer();
+
+  await container.read(settingsProvider.notifier).loadSettings();
+  await container.read(keyboardConfigProvider.notifier).loadConfig();
+
+  runApp(
+    UncontrolledProviderScope(
+      container: container,
+      child: const ArcheryHelperApp(),
+    ),
+  );
 }
